704645281d952e37af5784683563f943db2bd04a
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-05-15 22:07:52 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,71 +1,3 @@
-const dummyPurchases = [
-  {
-    shoeId: "SH001",
-    brand: "Nike",
-    gender: "Men",
-    catagory: "sneekers",
-    description: "Nike Air Zoom Pegasus 39",
-    url: "../assets/men_shoes/shoe_13.jpeg",
-    unitPrice: 17999,
-    discount: 10,
-    availableQty: 15,
-    quantity: 2,
-    size: 42,
-  },
-  {
-    shoeId: "SH002",
-    brand: "Adidas",
-    gender: "Women",
-    catagory: "sneekers",
-    description: "Adidas Ultraboost Light Running Shoes",
-    url: "../assets/women_shoes/shoe_11.jpeg",
-    unitPrice: 20999,
-    discount: 15,
-    availableQty: 10,
-    quantity: 1,
-    size: 38,
-  },
-  {
-    shoeId: "SH003",
-    brand: "Puma",
-    gender: "Unisex",
-    catagory: "sneekers",
-    description: "Puma Smash V2 Sneakers",
-    url: "../assets/men_shoes/shoe_1.jpg",
-    unitPrice: 12999,
-    discount: 5,
-    availableQty: 20,
-    quantity: 3,
-    size: 40,
-  },
-  {
-    shoeId: "SH004",
-    brand: "Reebok",
-    gender: "Men",
-    catagory: "sneekers",
-    description: "Reebok Flexagon Energy TR 3.0",
-    url: "../assets/boys_shoes/shoe_1.jpg",
-    unitPrice: 14999,
-    discount: 20,
-    availableQty: 12,
-    quantity: 1,
-    size: 43,
-  },
-  {
-    shoeId: "SH005",
-    brand: "Fila",
-    gender: "Women",
-    catagory: "sneekers",
-    description: "Fila Disruptor II Premium",
-    url: "../assets/girls_shoes/shoe_4.jpg",
-    unitPrice: 18999,
-    discount: 25,
-    availableQty: 8,
-    quantity: 2,
-    size: 39,
-  },
-];
-
 // Numbering format
 const formatCurrency = (value) =>
   new Intl.NumberFormat("en-LK", {
@@ -93,7 +25,6 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   /* Cart Data (modifiable) */
   let cartList = purchaseItem === null ? cartItems : [purchaseItem];
-  //let cartList = [...dummyPurchases]; // use `let` so we can reassign it
 
   /* Rendering - Cart Table Body */
   function generateCartHTML(item) {
