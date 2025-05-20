f93c4d97227a50820a8ee8376b3045d061fe015b
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-05-20 21:41:51 +0530

    Update sales.php

--- api/routes/sales.php
+++ api/routes/sales.php
@@ -14,6 +14,8 @@ if ($requestMethod === 'GET' && $subDirectory === "summary") {
     require_once __DIR__ . '/../controllers/sales.orders.php';
 } elseif ($requestMethod === 'PATCH' && $subDirectory === "insights") {
     require_once __DIR__ . '/../controllers/sales.updateOrder.php';
+} elseif ($requestMethod === 'GET' && $subDirectory === "store") {
+    require_once __DIR__ . '/../controllers/sales.getProducts.php';
 } else {
     http_response_code(405);
     header('Content-Type: application/json');
